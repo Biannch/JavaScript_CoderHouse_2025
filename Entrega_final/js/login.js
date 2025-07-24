3a99c2e8cc18ff6cd569bada71abf05a97540ef4
Bianca Chiaramello <chiaramellobianca@gmail.com>
2025-07-24 13:54:54 -0300

    avances

--- Entrega_final/js/login.js
+++ Entrega_final/js/login.js
@@ -3,18 +3,61 @@ document.addEventListener("DOMContentLoaded", () => {
 
   if (usuarioGuardado) {
     const usuario = JSON.parse(usuarioGuardado);
-    mostrarHomeBanking(usuario); // A definir después
-  } else {
     mostrarLogin();
+    // mostrarHomeBanking(usuario); // A definir después
+  } else {
+    mostrarSignUp();
   }
 });
 
-function mostrarLogin() {
+function mostrarSignUp(){
   const contenedor = document.getElementById("mainContainer");
 
   contenedor.innerHTML = `
-        <div class="login-container">
-            <form class="login-form">
+        <div class="inicio-container">
+            <form class="inicio-form" id="inicioForm">
+            <h2>Crear cuenta</h2>
+
+            <label for="nombre">Nombre de usuario</label>
+            <input type="text" id="nombre" required>
+
+            <label for="cuenta">Número de Cuenta</label>
+            <input type="text" id="cuenta" required>
+
+            <label for="pin">PIN</label>
+            <input type="password" id="pin" required>
+
+            <button type="submit">Ingresar</button>
+            </form>
+        </div>
+  `;
+
+  const form = document.getElementById("inicioForm");
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const nombre = document.getElementById("nombre").value;
+    const cuenta = document.getElementById("cuenta").value;
+    const pin = document.getElementById("pin").value;
+    const usuario = { nombre, cuenta, pin };
+
+    // Guardar en localStorage como sesión
+    localStorage.setItem("usuarioActual", JSON.stringify(usuario));
+
+    location.reload();
+
+    // // Guardar en usuarios.json -----------> pendiente
+
+
+    // mostrarHomeBanking(usuario);
+  })
+}
+
+function mostrarLogin(){
+  const contenedor = document.getElementById("mainContainer");
+
+  contenedor.innerHTML = `
+        <div class="inicio-container">
+            <form class="inicio-form" id="inicioForm">
             <h2>Inicio de Sesión</h2>
 
             <label for="cuenta">Número de Cuenta</label>
@@ -27,4 +70,6 @@ function mostrarLogin() {
             </form>
         </div>
   `;
+
+    // mostrarHomeBanking(usuario);
 }
